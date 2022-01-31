bc1cee7c297a1270ca130eeae08fe0909bbfc480
Rahul Jain <jainrahul0311@gmail.com>
2022-01-31 22:25:43 +0530

    Added StrStr

---
+++ src/main/java/com/practice/easy/StrStr.java
@@ -0,0 +1,25 @@
+package com.practice.easy;
+
+public class StrStr {
+
+    public int strStr(String haystack, String needle) {
+
+        if (haystack == null || needle == null)
+            return -1;
+        int l1 = haystack.length();
+        int l2 = needle.length();
+        for (int i = 0; i < l1-l2+1; i++) {
+            int count = 0;
+            while (count < l2 && haystack.charAt(i+count) == needle.charAt(count))
+                count++;
+            if (count == l2)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        StrStr str = new StrStr();
+        System.out.println("str = " + str.strStr("hello", "ll"));
+    }
+}
