a264ee5411edc91cc4aecf106cdd3e5da71a6e09
Rahul Jain <jainrahul0311@gmail.com>
2022-02-01 22:58:02 +0530

    Added Daily Temperature Monolithic Stack approach

--- src/main/java/com/practice/medium/DailyTemperatures.java
+++ src/main/java/com/practice/medium/DailyTemperatures.java
@@ -1,12 +1,13 @@
 package com.practice.medium;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class DailyTemperatures {
 
     public static void main(String[] args) {
         DailyTemperatures temperatures = new DailyTemperatures();
-        System.out.println("temperatures.dailyTemperatures(new int[]{73,74,75,71,69,72,76,73}) = " + Arrays.toString(temperatures.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
+        System.out.println("temperatures.dailyTemperatures(new int[]{73,74,75,71,69,72,76,73}) = " + Arrays.toString(temperatures.dailyTempMonoStack(new int[]{73, 74, 75, 71, 69, 72, 76, 73})));
     }
 
     public int[] dailyTemperatures(int[] t) {
@@ -27,6 +28,24 @@ public class DailyTemperatures {
 
         return res;
     }
-    
+
+    //Monotonic Stack
+    public int[] dailyTempMonoStack(int[] t){
+
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[t.length];
+
+        for (int i = 0; i < t.length; i++) {
+
+            while (!stack.isEmpty() && t[stack.peek()] < t[i]){
+                int tmp = stack.pop();
+                res[tmp] = i - tmp;
+            }
+
+            stack.push(i);
+        }
+
+        return res;
+    }
     
 }
